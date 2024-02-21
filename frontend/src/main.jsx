650821d69b604096aca654766ebf52d328d7b177
Ana <anabelenbernardez@gmail.com>
2024-02-21 02:07:06 +0100

    First translate component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,12 +6,42 @@ import { AuthProviderComponent } from "./context/AuthContext.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
+import { I18nextProvider } from "react-i18next";
+import i18n from "i18next";
+
+import engTranslation from "./languages/eng.json";
+import espTranslation from "./languages/esp.json";
+import ptTranslation from "./languages/pt.json";
+
+const resources = {
+  eng: {
+    translation: engTranslation,
+  },
+  esp: {
+    translation: espTranslation,
+  },
+  pt: {
+    translation: ptTranslation,
+  },
+};
+
+i18n.init({
+  resources,
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProviderComponent>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProviderComponent>
+    <I18nextProvider i18n={i18n}>
+      <AuthProviderComponent>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProviderComponent>
+    </I18nextProvider>
   </React.StrictMode>
 );
