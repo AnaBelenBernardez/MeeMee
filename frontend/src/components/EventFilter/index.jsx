18d5b2b3f2120f67d9d84f6a81d65df9e1f62a92
Ana <anabelenbernardez@gmail.com>
2024-02-29 17:23:32 +0100

    Better styles and animations for filters

--- frontend/src/components/EventFilter/index.jsx
+++ frontend/src/components/EventFilter/index.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import "./style.css";
 
 function EventFilter({ locations, onFilterChange }) {
   const { t } = useTranslation();
   const [selectedLocation, setSelectedLocation] = useState("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const selectRef = useRef(null);
 
   const handleLocationChange = (event) => {
     const location = event.target.value;
@@ -21,24 +23,50 @@ function EventFilter({ locations, onFilterChange }) {
       .join(" ");
   };
 
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (selectRef.current && !selectRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    }
+
+    document.body.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.body.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="filter-element">
-      <select
-        id="filter"
-        name="location"
-        value={selectedLocation}
-        onChange={handleLocationChange}
-      >
-        <option value="" disabled>
-          {t("translation.searchCity")}
-        </option>
-        <option value="">{t("translation.allCities")}</option>
-        {locations.map((location) => (
-          <option key={location} value={location}>
-            {formatLocation(location)}
-          </option>
-        ))}
-      </select>
+      <div className="custom-select-wrapper">
+        <div
+          className={`custom-select ${isDropdownOpen ? "open" : ""}`}
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          ref={selectRef}
+        >
+          <span>
+            {selectedLocation
+              ? formatLocation(selectedLocation)
+              : t("translation.searchCity")}
+          </span>
+          <ul className="custom-select-options">
+            <li onClick={() => handleLocationChange({ target: { value: "" } })}>
+              {t("translation.allCities")}
+            </li>
+            {locations.map((location) => (
+              <li
+                key={location}
+                onClick={() =>
+                  handleLocationChange({ target: { value: location } })
+                }
+              >
+                {formatLocation(location)}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
