1e9f50172a8af2a4d69b79bffb28b3117b023506
Ana <anabelenbernardez@gmail.com>
2024-03-04 16:55:55 +0100

    Animations for submit button, better translations

--- frontend/src/components/ArrowButton/index.jsx
+++ frontend/src/components/ArrowButton/index.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 function ArrowButton() {
+  const [clicked, setClicked] = useState(false);
+
+  const handleMouseDown = () => {
+    setClicked(true);
+    setTimeout(() => {
+      setClicked(false);
+    }, 5000);
+  };
+
+  const handleMouseUp = () => {
+    setClicked(false);
+  };
+
   return (
     <div className="arrow-element">
-      <button className="arrow-button">
+      <button
+        className={`arrow-button ${clicked ? "clicked" : ""}`}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      >
         <div className="triangles-container">
           <div className="black-triangle">
             <div className="green-triangle"></div>
